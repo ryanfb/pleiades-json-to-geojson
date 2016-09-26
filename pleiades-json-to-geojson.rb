cb7725c422261862c11c3d67a33111a23fa53883
Ryan Baumann <ryan.baumann@gmail.com>
2016-09-26 12:00:51 -0400

    Translate Pleiades place resources to GeoJSON features

--- pleiades-json-to-geojson.rb
+++ pleiades-json-to-geojson.rb
@@ -13,6 +13,22 @@ pleiades_geojson["type"] = "FeatureCollection"
 pleiades_geojson["features"] = []
 
 pleiades_json['@graph'].each do |place|
+  # translate Pleiades place resources to GeoJSON features
+  if place['reprPoint'] # omit places with no reprPoint
+    place_feature = {}
+    place_feature['type'] = 'Feature'
+    place_feature['bbox'] = place['bbox'] if place['bbox']
+    place_feature['geometry'] = {}
+    place_feature['geometry']['type'] = 'Point'
+    place_feature['geometry']['coordinates'] = place['reprPoint']
+    place_feature['properties'] = {}
+    place_feature['properties']['title'] = place['title']
+    place_feature['properties']['description'] = place['description']
+    place_feature['properties']['link'] = place['uri']
+    pleiades_geojson["features"] << place_feature
+  end
+
+  # translate Pleiades location resources to GeoJSON features
   place['features'].each do |feature|
     pleiades_geojson["features"] << feature
   end
